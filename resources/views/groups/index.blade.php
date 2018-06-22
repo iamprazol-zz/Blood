5d8933b79dac34151594d02fcd3ba87a9a319451
Prajjwal Poudel <iamprazol@gmail.com>
2018-06-22 18:01:42 +0545

    Bar chart was added

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-8 m-auto">
+        <div class="col-md-12 m-auto">
 
             <div class="card sm-hidden">
 
                 <div class="card-header">
                     <h4>Blood Groups</h4>
-
-                    <a href="{{ route('groups.search') }}" class="btn btn-outline-danger btn-sm pull-right">Search for a donor</a>
                 </div>
 
 
-                <div class="card-body">
+                <div class="card-body card-deck">
+                    <div class="col-md-5 m-auto">
 
-                        <table class="table table-hover">
+
+                    <table class="table table-hover">
 
                             <thead>
 
@@ -56,13 +56,79 @@
                             </tbody>
 
                         </table>
-
                     </div>
+                    <div class="col-md-25 m-auto">
+
+
+                    <canvas id="myChart" width="400px" height="400px"></canvas>
+
+                    <script src="/js/chart.bundle.js"></script>
+                    <script src="/js/utils.js"></script>
+
+
+                    <script>
+                        var ctx = document.getElementById("myChart").getContext('2d');
+                        var myChart = new Chart(ctx, {
+                            type: 'bar',
+                            data: {
+                                labels: ["AB-", "AB+", "B-", "B+", "A-", "A+" ,"O-" , "O+"],
+                                datasets: [{
+                                    label: 'No Of Donors',
+                                    data: [@foreach($groups as $group)
+                                        {{$group->user->count()}},
+                                        @endforeach
+                                    ],
+                                    backgroundColor: [
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(200, 189, 167, 0.2)',
+                                        'rgba(150, 255,132, 0.2)' ,
+
+                                    ],
+                                    borderColor: [
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(200, 189,167, 1)',
+                                        'rgba(150, 255,132, 1)' ,
+
+
+
+                                    ],
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero:true
+                                        }
+                                    }]
+                                }
+                            }
+                        });
+                    </script>
 
                 </div>
 
+
             </div>
 
+            </div>
         </div>
+    </div>
+
+
+
+
 
 @endsection
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,20 +77,12 @@
 
         <br>
 
-        <footer>
-            <nav class="navbar navbar-expand navbar-light bg-danger">
-                <div class="container">
 
-                    <p>copyright</p>
-
-                </div>
-            </nav>
-        </footer>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="js/shards.min.js"></script>
+    <script src="/js/shards.min.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script>
@@ -102,6 +94,18 @@
         @endif
 
     </script>
+
+@yield('footer')
+
+        <footer>
+            <nav class="navbar navbar-expand navbar-light bg-danger">
+                <div class="container">
+
+                    <p>copyright</p>
+
+                </div>
+            </nav>
+        </footer>
 
 </body>
 </html>
