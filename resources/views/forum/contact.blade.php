7fe4a9ec3a5c8d480f245b7b60dd32fac876b5a9
Prajjwal Poudel <iamprazol@gmail.com>
2018-06-26 11:50:01 +0545

    contact person image added

---
+++ resources/views/forum/contact.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+
+        <div class="row justify-content-center">
+            <div class="col-sm-20 md-auto">
+                <div class="">
+                    <title>Contacts</title>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div><h2 style="text-align: center;color:red">For any query, feel free to contact us.</h2><br/>
+                                <article>
+                                    <fieldset style="padding: 10px 10px; font-size: 20px">
+                                        <img src="/images/kabita.jpg" style="width: 155px; height: 150px; border-radius: 50%; float: left; clear: left">
+                                        <br/>
+                                        Email: <a href="#" target="">parajulikabita10@gmail.com</a><br/>
+                                        Facebook: <a href="https://www.facebook.com/kabita.parajuli.3990" target="">Kabita Parajuli</a>
+                                    </fieldset>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <article>
+                                <fieldset style="padding: 10px 10px; font-size: 20px">
+                                    <img src="/images/prajjwal.jpg" style="width: 155px; height: 150px; border-radius: 50%; float: left; clear: left">
+                                    Email: <a href="#" target="_blank">iamprajjwal@gmail.com</a><br/>
+                                    Facebook: <a href="https://www.facebook.com/ujrap" target="">प्रज्वल पौडेल</a>
+                                </fieldset>
+                            </article>
+                        </div>
+                    </div>
+
+                    <div class="container-fluid">
+                        <div class="row">
+                            <article>
+                                <fieldset style="padding: 10px 10px; font-size: 20px">
+                                    <img src="/images/rasilla.jpg" style="width: 155px; height: 150px; border-radius: 50%; float: left; clear: left">
+                                    Email: <a href="" target="_blank">rasilamichhane@gmail.com</a><br/>
+                                    Facebook: <a href="https://www.facebook.com/rashila.lamichhane.54" target="">Rashila Lamichhane</a>
+                                </fieldset>
+                            </article>
+                        </div>
+                    </div>
+
+                    <div class="container-fluid">
+                        <div class="row">
+                            <article>
+                                <fieldset style="padding: 10px 10px; font-size: 20px">
+                                    <img src="/images/anjaan.jpg" style="width: 155px; height: 150px; border-radius: 50%; float: left; clear: left"/>
+                                    Email: <a href="https://www.gmail.com" target="_blank">anjangaire@gmail.com</a><br/>
+                                    Facebook: <a href="https://www.facebook.com/anjaan.gaire" target="_blank">Sagar Gaire</a>
+                                </fieldset>
+                            </article>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+@endsection
+
+
